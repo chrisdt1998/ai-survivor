4944200d61fa0245c177d5495d3b99d8db5834df
Christopher du Toit (chto) <chto@odoo.com>
2026-09-24 13:28:10 +0200

    Agent and main

---
+++ agent/agent.py
@@ -0,0 +1,11 @@
+class Agent:
+    def __init__(self) -> None:
+        pass
+
+    def get_state(self, core):
+        # Get the agent position
+        # Get the enemies positions
+        # Get the power up positions
+        # discretize.
+        player = core.get_entity_by_name('player')
+        pass
---
+++ main.py
@@ -0,0 +1,19 @@
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test', help='Test the model', required=False, default=False, action='store_true')
+    parser.add_argument('--train', help='Train the model', required=False, default=False, action='store_true')
+    parser.add_argument('--render', help='Run and show the renders', required=False, default=False, action='store_true')
+
+    args = parser.parse_args()
+
+    if args.test and args.train:
+        parser.error('Cannot be both test and train at the same time.')
+
+    if not args.test and not args.train:
+        logger.warning('No mode set, using test by default.')
